b3fb0901886a180d905d618f7c6a76f91294dd5d
Nandoby <nandobiaccalli@gmail.com>
2021-09-19 14:31:52 +0200

    création de la liste de jeux sur la page home

--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,11 @@
 <h2 class="home_title">Listing de mes jeux</h2>
 
+<ul>
+    <?php
+        $req = $bdd->query('SELECT * FROM jeuxvideos');
+        while ($don = $req->fetch())
+        {
+            echo '<li class="game_list"><a href="index.php?action=game.php&id='.$don['id'].'">'.$don['name'].'</a></li>';
+        }
+    ?>
+</ul>
