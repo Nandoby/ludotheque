ac28c649076633b34bf6ad347407044fce66402a
Nandoby <nandobiaccalli@gmail.com>
2021-09-25 20:46:38 +0200

    traitement de la modification si un fichier a été uploadé

--- admin/treatmentModify.php
+++ admin/treatmentModify.php
@@ -7,6 +7,15 @@ if (!isset($_SESSION['login'])) {
 if (isset($_GET['id'])) {
     $id = htmlspecialchars($_GET['id']);
 }
+require '../connexion.php';
+$req = $bdd->prepare('SELECT * FROM jeuxvideos WHERE id = ?');
+$req->execute([$id]);
+if (!$don = $req->fetch())
+{
+    $req->closeCusro();
+    header("LOCATION:games.php");
+}
+$req->closeCursor();
 
 if (isset($_POST['nom']) && !empty($_POST['nom'])) {
     require '../connexion.php';
@@ -54,12 +63,77 @@ if (isset($_POST['nom']) && !empty($_POST['nom'])) {
                     "jeux" => $id
                 ]);
             }
-
-
-
             header('LOCATION:modify.php?id=' . $id . '&success=true');
         } else {
+            // Ici je traite mon image uploadé
+            $dossier = '../images/';
+            $image = basename($_FILES['file']['name']);
+            $imageSize = filesize($_FILES['file']['tmp_name']);
+            $extensions = ['.jpg', '.jpeg', '.png'];
+            $imageExtension = strrchr($_FILES['file']['name'], '.');
+            $tailleMax = 2000000;
 
+            // Test de l'extension du fichier
+            if (!in_array($imageExtension, $extensions))
+            {
+                $fileError = 'wrong-extension';
+            }
+
+            // Test de la taille de l'image
+            if ($imageSize > $tailleMax)
+            {
+                $fileError = 'wrong-size';
+            }
+
+            if (!isset($fileError))
+            {
+                // Formatage du nom de fichier envoyé
+                $image = strtr($image,
+                    'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
+                    'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy'
+                );
+
+                // remplacement des caractères spéciaux
+                $image = preg_replace('/([^.a-z0-9]+)/i', '-', $image);
+
+                // Traitement des fichiers doublons
+                $imagecpt = rand() . $image;
+
+                if (move_uploaded_file($_FILES['file']['tmp_name'], $dossier . $imagecpt))
+                {
+                    unlink("../images/".$don['image']);
+                    $update = $bdd->prepare('UPDATE jeuxvideos SET image = :img, name = :name, description = :desc, prix = :prix, release_date = :date, editeur = :editeur WHERE id = :id');
+                    $update->execute([
+                        'name' => $nom,
+                        'desc' => $desc,
+                        'prix' => $prix,
+                        'date' => $date,
+                        'editeur' => $editeur,
+                        'id' => $id,
+                        'img' => $imagecpt
+                    ]);
+                    $update->closeCursor();
+                    // Je supprime tous les genres_jeux du jeux
+                    // Ensuite j'ajoute les genres que j'ai coché
+
+                    $deleteGenres = $bdd->prepare('DELETE FROM genres_jeux WHERE id_jeux = ?');
+                    $deleteGenres->execute([$id]);
+                    $deleteGenres->closeCursor();
+
+                    foreach($genres as $gen)
+                    {
+                        $insert = $bdd->prepare('INSERT INTO genres_jeux (id_genre, id_jeux) VALUES(:genre, :jeux)');
+                        $insert->execute([
+                            "genre" => $gen,
+                            "jeux" => $id
+                        ]);
+                    }
+
+
+                };
+            } else {
+                header("LOCATION:modify.php?id=$id&update=error");
+            }
         }
     }
 
